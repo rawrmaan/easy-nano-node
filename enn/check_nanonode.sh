5aec45be63f721c1bb3e2ce3a1c92c8d867f411a
Arman Dezfuli-Arjomandi <rawrmaan@gmail.com>
2018-07-02 19:21:05 -0400

    Remove unnecessary code

--- enn/check_nanonode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-response=$(curl --connect-timeout 3 --max-time 5 -g -d '{ "action": "version" }' [::1]:7076);
-if [ $? -ne 0 ]; then
-    docker restart enn_nanonode_1
-fi;
--- enn/setup.sh
+++ enn/setup.sh
@@ -9,31 +9,6 @@ sudo docker pull nanocurrency/nano
 echo "== Starting Docker containers"
 sudo docker-compose up -d
 
-echo "== Take a deep breath..."
-# we need this as the node is crashing if we go on too fast
-sleep 5s
-
-# # check if init already done
-# if [ -f /opt/easy-nano-node/init ]; then
-
-#   echo "== Initialization already done, skipping ..."
-
-# else
-
-#   # echo "== Opening Nano Node Port"
-#   # sudo ufw allow 7075
-
-#   # echo "== Opening RPC Port"
-#   # sudo ufw allow 80
-
-#   echo "== Restarting Nano node container"
-#   sudo docker restart enn_nanonode_1
-
-#   # we're done, save for later
-#   touch /opt/easy-nano-node/init
-
-# fi
-
 # get that nasty IP
 serverip=$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')
 
