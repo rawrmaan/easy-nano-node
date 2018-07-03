6560986b9cf7310e8d90702baaeebe5f23b19532
Arman Dezfuli-Arjomandi <rawrmaan@gmail.com>
2018-07-02 23:55:21 -0400

    Add proxy service

--- proxy/setup.sh
+++ proxy/setup.sh
@@ -5,3 +5,7 @@ cd "$(dirname "$0")"
 
 echo "== Starting Docker containers"
 sudo docker-compose up -d
+
+# install service
+sudo cp ./docker-compose-app.service /etc/systemd/system/docker-compose-app.service
+sudo systemctl enable docker-compose-app
